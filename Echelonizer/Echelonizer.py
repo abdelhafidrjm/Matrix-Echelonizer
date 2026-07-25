d57b246fc3812319052b8eda1f9fc1a4fffbcc6a
Abdelhafid <abdelhafid.redjem07@gmail.com>
2026-07-25 05:21:28 +0100

    seperated the two branches calculator and gui for echelonizer

--- Echelonizer/Echelonizer.py
+++ Echelonizer/Echelonizer.py
@@ -1,11 +1,3 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.geometry("600x200")
-root.title("Matrix Echelonizer")
-root.config(background="#5094BE")
-
-
 def echelon(n , m, mat):
 # initialization
     k = 0
@@ -18,7 +10,7 @@ def echelon(n , m, mat):
         # check if pivot is null
         while pivot == 0:
             l = k
-            while l < n and mat[l][p] == 0:
+            while mat[l][p] == 0 and l < n:
                 l += 1 
                 
             if l < n:
@@ -50,105 +42,11 @@ def echelon(n , m, mat):
 # Return the reduced matrix
     return mat
         
-# response = 0
-def ech(response):
-    if response == 1 or response == 2:
-        n = 3
-        m = 3
-        mat = [
-            [0, 1, 2],
-            [7, 0, 4],
-            [0, 5, 6]
-        ]
-
-        # TEMPORARRY
-        # print("Je suis désolé car je n'ai pas encore ajouté d'interface dynamique, donc nous allons nous en tenir à la deuxième option.")
-        
-        # print("\n-------matrice-------")
-        # for i in range(n):
-        #     for j in range(m):
-        #         print(f"|{mat[i][j]:+.2f}", end="|")
-        #     print("\n---------------------")
-
-        # matEchelone = echelon(n, m, mat)
-        # print("\n--matrice échelonnée-")
-        # for i in range(n):
-        #     for j in range(m):
-        #         print(f"|{matEchelone[i][j]:+.2f}", end="|")
-        #     print("\n---------------------")
-            
-            
-        for i in range(n):
-            for j in range(m):
-                # Création du label avec les coordonnées en texte
-                elementij = mat[i][j]
-                element = tk.Label(root, text = elementij, borderwidth = 1, relief="solid", width = 10, height = 2)
-                element.grid(row = i, column = j, padx = 5, pady = 5)
-
-            
-        bye.pack(anchor = "w")
-    else:
-        bye.pack(anchor = "w")
-        
-# def onClick(resp):
-#     global response
-#     response = resp
-    
     
 
-
-
-bienvenue = tk.Label(root, 
-                  text = "Bienvenue à l'echelonizer matriciel calculator!\nSouhaitez-vous", 
-                  bg = "#5094BE", 
-                  fg = "#fff", 
-                  font = ("Helvetica", 20)
-                  ).pack(anchor = "w")
-
-
-
-button1 = tk.Button(root, 
-                    text = "1) échelonner une matrice?", 
-                    command = lambda : ech(1)
-                    ).pack(anchor = "w")
-
-button2 = tk.Button(root, 
-                    text = "2) voir comment le programme fonctionne?", 
-                    command = lambda : ech(2)
-                    ).pack(anchor = "w")
-
-button3 = tk.Button(root, 
-                    text = "3) quitter",
-                    command = lambda : ech(3)
-                    ).pack(anchor = "w")
-
-element = tk.Label(root, text = "Matrice : ")
-
-bye = tk.Label(root, 
-                  text = "au revoir", 
-                  bg = "#5094BE", 
-                  fg = "#fff", 
-                  font = ("Helvetica", 20)
-                  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Bienvenue à l'echelonizer matriciel calculator!")
+print("Souhaitez-vous \n 1) échelonner une matrice? \n 2)Voir comment le programme fonctionne? \n 3)Quitter")
+response = int(input("Select (1 or 2): "))
 
 #match response:
     # case 1:
@@ -160,6 +58,29 @@ bye = tk.Label(root,
     #         for j in range(m):
     #             mat[i][j] = input(f"a{i}{j} = ")
         
+if response == 1 or response == 2:
+        n = 3
+        m = 3
+        mat = [
+            [0, 1, 2],
+            [7, 0, 4],
+            [0, 5, 6]
+        ]
 
+        # TEMPORARRY
+        print("Je suis désolé car je n'ai pas encore ajouté d'interface dynamique, donc nous allons nous en tenir à la deuxième option.")
+        
+        print("\n-------matrice-------")
+        for i in range(n):
+            for j in range(m):
+                print(f"|{mat[i][j]:+.2f}", end="|")
+            print("\n---------------------")
 
-
+        matEchelone = echelon(n, m, mat)
+        print("\n--matrice échelonnée-")
+        for i in range(n):
+            for j in range(m):
+                print(f"|{matEchelone[i][j]:+.2f}", end="|")
+            print("\n---------------------")
+            
+print("bye-bye")
