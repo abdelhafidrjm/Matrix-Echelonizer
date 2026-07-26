f3426bd35c5418a20383e8ae3fb626099649e55f
Abdelhafid <abdelhafid.redjem07@gmail.com>
2026-07-26 04:26:48 +0100

    fixed some bugs, il me reste le PEDMAS et les nombres negatifs pour maintenant

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -21,24 +21,36 @@ def refresh(n):
         n = "π" 
     elif n == ".":
         n = "."
-    elif n == 0:
-        n = ""
     else:
         n = int(n) if n == round(n) else n
     
     textbox.config(text = n)
     
 def clear(n):
-    global Number, op, Numbers, ops
-    Number = n
+    global Number, op, Numbers, ops, insertNumber
     Numbers.clear()
-    op = ""
+    Number = n
     ops.clear()
+    insertNumber = iN
+    op = ""
 
 
 def backspace():
     global Number
-    Number = (Number - Number % 10) / 10
+    if insertNumber == iN:
+        if Number < 0:
+            Number *= -1 
+            Number = (Number - Number % 10) / 10
+            Number *= -1
+        else:
+            Number = (Number - Number % 10) / 10
+    else:
+        count = 0
+        while Number != round(Number):
+            count += 1
+            Number *= 10
+        Number = (Number - Number % 10) / 10
+        Number /= 10 ** (count-1)
     refresh(Number)
 
 def delete():
@@ -49,12 +61,13 @@ def delete():
     
 # Calculator functions
 def iN(num):
-    global Number
-    if num == pi:
+    global Number, Numbers
+    if Number == pi or num == pi:
         Number = num
     else:
         Number = Number * 10 + num
     refresh(Number)
+    
 insertNumber = iN
     
 def iDe(num):
@@ -68,12 +81,14 @@ def iDe(num):
 
 
 def operationSaver(ope):
-    global ops, op, Numbers, Number, insertNumber
+    global ops, op, Numbers, Number, insertNumber, countD
     if ope == ".":
-        Number = float(Number/1)
+        countD = 0
+        Number = float(Number)
         insertNumber = iDe
-        refresh(ops[i])
+        refresh(ope)
     elif ope != "=" : 
+        insertNumber = iN
         op = ope
         ops.append(op)
         Numbers.append(Number)
@@ -88,7 +103,10 @@ def operationSaver(ope):
                 case "+":
                     result += Numbers[i+1]
                 case "-":
+                    # if len(Numbers) != len(ops):
                     result -= Numbers[i+1]
+                    # else:
+                    #     result *= - Numbers[i+1]
                 case "x":
                     result *= Numbers[i+1]    
                 case "/":
@@ -111,57 +129,29 @@ textbox.pack()
 theGrid = Frame(root)
 
 # Numbers
-buttonnumber1 = Button(theGrid, height = 2, text = 1,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(1),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber2 = Button(theGrid, height = 2, text = 2,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(2),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber3 = Button(theGrid, height = 2, text = 3,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(3),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber4 = Button(theGrid, height = 2, text = 4,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(4),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber5 = Button(theGrid, height = 2, text = 5,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(5),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber6 = Button(theGrid, height = 2, text = 6,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(6),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber7 = Button(theGrid, height = 2, text = 7,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(7),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber8 = Button(theGrid, height = 2, text = 8,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(8),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber9 = Button(theGrid, height = 2, text = 9,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(9),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumber0 = Button(theGrid, height = 2, text = 0,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(0),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumberp = Button(theGrid, height = 2, text = "π", font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(pi),   bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
-buttonnumberd = Button(theGrid, height = 2, text = ".", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("."),  bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED)
+k = 0
+for i in [0,1,2,3]:
+    for j in [0, 1, 2]:
+        k += 1
+        if   k < 10  : Button(theGrid, height = 2, text = k,   font = ("Ink Free", 15, "bold"), command = lambda k = k: insertNumber(k),    bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED).grid(row = i, column = j,  padx = 1, pady = 1)
+        elif k == 10 : Button(theGrid, height = 2, text = ".", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("."),     bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED).grid(row = i, column = j,  padx = 1, pady = 1)
+        elif k == 11 : Button(theGrid, height = 2, text = 0,   font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(0),         bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED).grid(row = i, column = j,  padx = 1, pady = 1)
+        elif k == 12 : Button(theGrid, height = 2, text = "π", font = ("Ink Free", 15, "bold"), command = lambda : insertNumber(pi),        bg = "#9A3131", width = 8, borderwidth=5, relief= RAISED).grid(row = i, column = j,  padx = 1, pady = 1)
 
 # Arethmetic Operations
-buttonpls = Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "+", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("+"), width = 10)
-buttonmin = Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "-", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("-"), width = 10)
-buttonmul = Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "x", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("x"), width = 10)
-buttondiv = Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "/", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("/"), width = 10)
-buttoneql = Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "=", font = ("Ink Free", 15, "bold"), command = lambda : operationSaver("="), width = 10)
+for i, j, l in zip(["+", "-", "x", "/", "="], [1, 1, 2, 2, 3], [3, 4, 3, 4, 3]):
+    Button(theGrid, height = 2, bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = i, font = ("Ink Free", 15, "bold"), command = lambda k = i: operationSaver(k), width = 10).grid(row = j, column = l,  padx = 1, pady = 1)
 
 # Functionalities
 buttonbackspace = Button(theGrid,   height = 2, width = 11,bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "Backspace", font = ("Ink Free", 15), command = backspace)
 buttondelete = Button(theGrid,      height = 2, width = 11,bg = "#2427df", fg = "#fff",borderwidth = 5, relief = RAISED, text = "Delete", font = ("Ink Free", 15), command = delete)
-
 buttonclose = Button(theGrid,text="Quit", font = ("Ink Free", 15), command=root.destroy)
-
-# Displaying the buttons
-buttonnumber1.grid(row = 0, column = 0,  padx = 1, pady = 1)
-buttonnumber2.grid(row = 0, column = 1,  padx = 1, pady = 1)
-buttonnumber3.grid(row = 0, column = 2,  padx = 1, pady = 1)
-buttonnumber4.grid(row = 1, column = 0,  padx = 1, pady = 1)
-buttonnumber5.grid(row = 1, column = 1,  padx = 1, pady = 1)
-buttonnumber6.grid(row = 1, column = 2,  padx = 1, pady = 1)
-buttonnumber7.grid(row = 2, column = 0,  padx = 1, pady = 1)
-buttonnumber8.grid(row = 2, column = 1,  padx = 1, pady = 1)
-buttonnumber9.grid(row = 2, column = 2,  padx = 1, pady = 1)
-buttonnumber0.grid(row = 3, column = 1,  padx = 1, pady = 1)
-buttonnumberp.grid(row = 3, column = 2,  padx = 1, pady = 1)
-buttonnumberd.grid(row = 3, column = 0,  padx = 1, pady = 1)
-
-buttonmin.grid(row = 1, column = 4,  padx = 1, pady = 1, sticky = "w")
-buttonpls.grid(row = 1, column = 3,  padx = 1, pady = 1, sticky = "e")
-buttonmul.grid(row = 2, column = 3,  padx = 1, pady = 1, sticky = "e")
-buttondiv.grid(row = 2, column = 4,  padx = 1, pady = 1, sticky = "w")
-buttoneql.grid(row = 3, column = 3,  padx = 1, pady = 1, sticky = "e")
 
 buttonbackspace.grid(row = 0, column = 3,  padx = 1, pady = 1, sticky="e")
 buttondelete.grid(row = 0, column = 4,  padx = 1, pady = 1, sticky = "w")
 buttonclose.grid(row = 4, column = 4, sticky = "es")
 
-theGrid.config(relief = SOLID, borderwidth = 10, padx = 10, pady = 10, bg = "#000", height = 300)
+theGrid.config(relief = SOLID, borderwidth = 10, padx = 10, pady = 10, bg = "#000")
 theGrid.pack(fill = "x")
 
 
---
+++ Calculator/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# buttonpls.grid(row = 1, column = 3,  padx = 1, pady = 1, sticky = "e")
+# buttonmin.grid(row = 1, column = 4,  padx = 1, pady = 1, sticky = "w")
+# buttonmul.grid(row = 2, column = 3,  padx = 1, pady = 1, sticky = "e")
+# buttondiv.grid(row = 2, column = 4,  padx = 1, pady = 1, sticky = "w")
+# buttoneql.grid(row =
